1ea09ad892f9b42dc963adc44a28441be8305f0f
Rendy Andriyanto <rendyandriyanto@Rendys-MacBook-Pro.local>
2019-08-25 22:58:00 +0700

    add test.js

---
+++ test.js
@@ -0,0 +1,82 @@
+// // console.log('hello')
+
+//==== PANGKAT 3 ====//
+// const pangkatn  = (x, n) => {
+// 	return x ** n;
+// }
+// console.log(pangkatn(3, 3));
+
+
+//==== PANGKAT N ====//
+// const pangkat3  = (x) => {
+// 	return x ** 3;
+// }
+// console.log(pangkat3(3));
+
+
+//==== PHYTAGORAS ====/
+const phtyagoras = (x, y) => {
+	return Math.sqrt(x * x + y * y);
+}
+console.log(phtyagoras(2,3));
+
+
+
+//==== LEBIH BESAR ====/
+// const lebihbesar = (x) => {
+// 	if(x < 10){
+//         return 'true';
+//     }else{
+//         return 'false';
+//     }
+// }
+// console.log(lebihbesar(5));
+
+
+//============================================================//
+
+// function pythagorean_theorem(x, y) {
+//   if ((typeof x !== 'number') || (typeof y !== 'number')) 
+// return false; 
+// 		return Math.sqrt(x * x + y * y);
+// 	}
+
+// console.log(pythagorean_theorem(2, 4));
+// console.log(pythagorean_theorem(3, 4));
+
+// const feedTheCat = (cat) => {
+//   if (cat === 'hungry') {
+//     return 'Feed the cat';
+//   } else {
+//     return 'Do not feed the cat';
+//   }
+// }
+// console.log(feedTheCat());
+
+// function(a){
+//     if(a < 10){
+//         return 'valid';
+//     }else{
+//         return 'invalid';
+//     }
+// }
+
+// const a => (a < 10) ? 'valid' : 'invalid'
+// const a = (a < 10) => {
+// 	? 'valid' : 'invalid';
+// }
+
+// const pangkatn = (x, n) => {
+// 		 x = 5;
+// 		 n = 2;
+// 		 	//  y = x ** n;
+// 			// return  y;
+	  
+// };
+// console.log(pangkatn(x + n));
+
+// var add = (x,y) => {
+//   return x + y;
+// }
+// console.log(add(3, 3));
+
